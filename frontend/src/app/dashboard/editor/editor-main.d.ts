dd657bb3a942d906c7b94dc80af45c803420e96f
Rusu Cristian <ruscris2@gmail.com>
2018-12-24 16:35:14 +0200

    WIP code for Edtior renderer.

---
+++ frontend/src/app/dashboard/editor/editor-main.d.ts
@@ -0,0 +1,5 @@
+
+export default class Renderer {
+  Init(): any;
+  RenderLoop(loopFeedback): any;
+}
--- frontend/src/app/dashboard/editor/editor.component.ts
+++ frontend/src/app/dashboard/editor/editor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Renderer from './editor-main'
+
+declare var InitEditor: any;
 
 @Component({
   selector: 'app-editor',
@@ -10,6 +13,8 @@ export class EditorComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const rendererInstance = new Renderer();
+    rendererInstance.Init();
   }
 
 }
